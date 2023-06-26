f2ce96bcebf62ab6e0dcff5540ff7cbdb21c927d
MnariAsma <132150388+MnariAsma@users.noreply.github.com>
2023-06-26 02:39:03 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import { createRoot} from 'react-dom/client'
+// ReactDOM.render(<App />, document.getElementById('root'))
+createRoot(document.getElementById('root')).render(<App />);
+
+
+
+
+
